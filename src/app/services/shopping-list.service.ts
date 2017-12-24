5135938ef16f6c27068caf244be03a81b28e6cae
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2017-12-24 15:28:29 +0100

    Services.

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -20,6 +20,6 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 }
