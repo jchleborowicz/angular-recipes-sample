e252ab4aa65709729545fedc1a9833771087b551
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2017-12-24 11:13:30 +0100

    Services.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,11 +9,6 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
-
   @Output() recipeSelected = new EventEmitter<Recipe>();
 
   constructor() { }
---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from '../recipes/recipe.model';
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  constructor() { }
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+}
