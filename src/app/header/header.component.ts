e992a6b1611cfce2f9112b5a3b4215c90c439247
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2017-12-27 16:23:47 +0100

    Services.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,8 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
-  @Output() viewChanged = new EventEmitter<string>();
-
-  onSelect(viewId: string) {
-    this.viewChanged.emit(viewId);
-  }
-
 }
