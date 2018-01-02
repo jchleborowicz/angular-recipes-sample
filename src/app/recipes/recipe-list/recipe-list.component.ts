653d930f21854e53b64522f91e63a1d9cadeba93
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-01-02 23:26:16 +0100

    Editing a recipe.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../../services/recipe.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
   recipes: Recipe[];
+
+  private subscription: Subscription;
 
   constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
-    this.recipeService.recipesChanged.subscribe(value => this.recipes = value);
+    this.subscription = this.recipeService.recipesChanged.subscribe(value => this.recipes = value);
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
