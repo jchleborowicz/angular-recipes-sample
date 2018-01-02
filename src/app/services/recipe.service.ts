8a61a8a02dbd2c4db7a763cb76e3b8ae69a9d4a5
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-01-02 19:42:05 +0100

    Editing a recipe.

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -40,4 +40,12 @@ export class RecipeService {
   getById(id: number) {
     return this.recipes[id];
   }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+  }
 }
